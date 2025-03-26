b678140ad0b210a86443f2dd27028a1c69bd6464
laurenjacobonii <143565048+laurenjacobonii@users.noreply.github.com>
2025-03-26 20:21:06 +0000

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,55 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function MagicButton() {
+const funFacts = [
+  { id: 1, title: "The Great Wall of China is not visible from space with the naked eye.", isTrue: true },
+  { id: 2, title: "The human brain continues to grow and develop until around the age of 25.", isTrue: true },
+  { id: 3, title: "The average person spends more time on their phone than they do sleeping.", isTrue: false },
+];
+
+
+function FunFact() {
   return (
-    <button>Magic</button>
+    <div>
+      <h2>Fun Fact!</h2>
+      <ul>
+        {funFacts.map((fact) => (
+          <li 
+          key={fact.id}
+          style={{
+            color: fact.isTrue ? 'green' : 'red'
+          }}
+          >
+            {fact.title}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
+
+function EmailEntry() {
+  return (
+    <form action="Email">Enter your email:
+        <input type="email"></input>
+    </form>
+  )
+}
+
+function RandomComp() {
+  return (
+    <div>
+      <h1>Did you have a good day?</h1>
+      <div>
+        <button>Yes!</button>
+        <button>No.</button>
+      </div>
+    </div>
+  )
+
+}
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -33,7 +77,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <MagicButton />
+      <EmailEntry />
+      <FunFact />
+      <RandomComp />
     </>
   )
 }
